b1cc8974c20342f6818f6a898a6c5486bd5675bf
YOSHITAKA NAKASHIMA <>
2020-12-18 11:55:14 +0900

    [fix] DB category data

--- database/migrations/2020_08_27_190828_create_posts_table.php
+++ database/migrations/2020_08_27_190828_create_posts_table.php
@@ -17,7 +17,6 @@ class CreatePostsTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('body');
-            $table->bigInteger('category_id');
             $table->bigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
