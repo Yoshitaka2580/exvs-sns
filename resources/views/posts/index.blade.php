4adf37440a459cd002f2eea2a310b7a46bd88cf3
YOSHITAKA NAKASHIMA <>
2020-12-18 13:09:58 +0900

    [remove] nav_tags file

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,23 @@
 
 @section('content')
 @include('nav')
-@include('nav_tags')
+<nav class="tags-nav">
+  <div class="cost-wrapper">
+    <div class="container cost-container">
+      <div class="cost-item">
+        <a class="cost-link btn-backred" href="{{ route('posts.index') }}">All</a>
+        <a class="cost-link btn-backred" href="{{ route('tags.show', ['name' => 3000]) }}">3000</a>
+        <a class="cost-link btn-backred" href="{{ route('tags.show', ['name' => 2500]) }}">2500</a>
+        <a class="cost-link btn-backred" href="{{ route('tags.show', ['name' => 2000]) }}">2000</a>
+        <a class="cost-link btn-backred" href="{{ route('tags.show', ['name' => 1500]) }}">1500</a>
+        <form action="/search" method="get">
+          <input type="search" name="search" required class="tagInput" placeholder="タグを検索">
+          <button type="submit" class="btn-search btn-backred"><i class="fas fa-search"></i></button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
 @if (session('flash_message'))
 <div class="flash_message card-text text-center alert alert-danger">
   {{ session('flash_message') }}
