9ef094d833072616c7c5ee7082ec114213ba3e1b
YOSHITAKA NAKASHIMA <>
2021-01-16 04:15:20 +0900

    [update] マイページスタイル変更7

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -1,20 +1,21 @@
 @csrf
 <div class="user-edit">
 @if(!empty($user->thumbnail))
-  <img src="/storage/user/{{ $user->thumbnail }}" class="editThumbnail">
+  <img src="/storage/user/{{ $user->thumbnail }}" class="mypage-thumbnail">
 @else
-  <div class="card-user">画像なし</div>
+  <i class="fas fa-user mypage-editThumbnail"></i>
+  <p class="mt-2">画像なし</p>
 @endif
 </div>
 
 <div class="md-form">
   <input type="hidden" name="user_id" value="{{ $user->id }}" class="form-control">
-  <h3 class="card-user user-edit">{{ $user->name }}</h3>
+  <h3 class="user-edit">{{ $user->name }}</h3>
 </div>
 <div class="form-group">
   <textarea class="form-control" name="body" row="12" placeholder="自己紹介">{{ $user->body ?? old('body') }}</textarea>
 </div>
-<div class="form-group user-edit card-user">
+<div class="form-group user-edit">
   <label>プロフィール画像</label>
   <input type="file" name="thumbnail">
 </div>
