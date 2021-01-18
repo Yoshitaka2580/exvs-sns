75231da32cd26a390dc321f4e908236c357f008e
YOSHITAKA NAKASHIMA <>
2021-01-18 16:26:54 +0900

    [add] footerを追加

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container footer-container">
+    <p class="footer-title"><i class="fab fa-playstation"></i>VS-Connect</p>
+    <p class="footer-text">
+      <a href="https://twitter.com/yossy2580" class="mySocial-link">
+        <i class="fab fa-twitter"></i>作者のTwitter
+      </a>
+    </p>
+    <p class="copy-right">Copyright ©︎ yossy 2021 All Rights Reserved.</p>
+  </div>
+</footer>
